0f4ae61c6668a2be482ba71cbdaacdbc4bd62ff5
dohyunkim <kdh951102@gmail.com>
2022-05-24 21:14:43 +0900

    Add mosaic func

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ import cv2
 
 img = cv2.imread("person.jpg")
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+mosaic = cv2.cvtColor(img, cv2.COLOR_BRGA)
 
 cv2.imshow("Over the Clouds", img)
 cv2.imshow("Over the Clouds - gray", gray)
+cv2.imshow("Over the Clouds - mosaic", mosaic)
 
 dst = cv2.blur(img, (9, 9), anchor=(-1, -1), borderType=cv2.BORDER_DEFAULT)
 cv2.imshow("Over the Clouds - blur", dst)
