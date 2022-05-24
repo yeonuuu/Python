4bb7386acdbaf76df0ab04f926315a070d7dd03d
국연우 <yeonwoo@gug-yeon-uui-MacBookAir.local>
2022-05-24 21:03:14 +0900

    Add blur

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import cv2
 
-print("OpenCV version:")
-print(cv2.__version__)
-
-img = cv2.imread("clouds.jpg")
+img = cv2.imread("person.jpg")
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 cv2.imshow("Over the Clouds", img)
-cv2.imshow("Over the Clouds - gray", gray)
+cv2.imshow("Over the Clouds - grayyyy", gray)
+
+dst = cv2.blur(img, (9, 9), anchor=(-1, -1), borderType=cv2.BORDER_DEFAULT)
+cv2.imshow("Over the Clouds - blur", dst)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
